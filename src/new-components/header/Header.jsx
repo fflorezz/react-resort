40dabad28fb523fe3d66c9a6bf489d5e0ddb8809
fflorezz <felipeflorezart@gmail.com>
2020-05-13 19:28:34 -0500

    added nav component

--- src/new-components/header/Header.jsx
+++ src/new-components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../images/logo_transparent_white.png";
+import Menu from "./../menu/Menu";
+import Nav from "./../nav/Nav";
 
 import styles from "./header.module.scss";
-import logo from "../../images/logo_transparent_white.png";
-import { Link } from "react-router-dom";
-import BurgerBtn from "./../burger-btn/BurgerBtn";
-import Menu from "./../menu/Menu";
 
 const Header = () => {
   return (
@@ -13,6 +14,7 @@ const Header = () => {
         <img className={styles.logo} src={logo} alt="" />
       </Link>
       <Menu />
+      <Nav />
     </div>
   );
 };
---
+++ src/new-components/nav/Nav.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../images/logo_transparent_white.png";
+import heroImg from "../../images/hero-web.jpg";
+
+import styles from "./nav.module.scss";
+
+const Nav = () => {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.navMask}>
+        <img className={styles.navLogo} src={logo} alt="aurora" />
+        <div className={styles.container}>
+          <ul className={styles.navItems}>
+            <li className={styles.navItem}>
+              <Link to="/rooms">
+                <span>ROOMS</span>
+              </Link>
+            </li>
+          </ul>
+          <ul className={styles.contactItems}>
+            <li className={styles.contactItem}>
+              <a href="">
+                <span>contacto</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.navImage}>
+          <img src={heroImg} alt="" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
