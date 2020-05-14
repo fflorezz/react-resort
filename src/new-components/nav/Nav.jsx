c4c3ba97ff80c996e1fc116a6459866fadd0f04a
fflorezz <felipeflorezart@gmail.com>
2020-05-14 10:42:48 -0500

    added nav component

--- src/new-components/nav/Nav.jsx
+++ src/new-components/nav/Nav.jsx
@@ -11,8 +11,26 @@ const Nav = () => {
     <nav className={styles.nav}>
       <div className={styles.navMask}>
         <img className={styles.navLogo} src={logo} alt="aurora" />
-        <div className={styles.container}>
+        <div className={styles.navContainer}>
+          <ul className={styles.subItems}>
+            <li className={styles.subItem}>
+              <a href="">Contacto</a>
+            </li>
+            <li className={styles.subItem}>
+              <a href="">Reservas</a>
+            </li>
+          </ul>
           <ul className={styles.navItems}>
+            <li className={styles.navItem}>
+              <Link to="/rooms">
+                <span>ROOMS</span>
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/rooms">
+                <span>ROOMS</span>
+              </Link>
+            </li>
             <li className={styles.navItem}>
               <Link to="/rooms">
                 <span>ROOMS</span>
@@ -20,6 +38,16 @@ const Nav = () => {
             </li>
           </ul>
           <ul className={styles.contactItems}>
+            <li className={styles.contactItem}>
+              <a href="">
+                <span>contacto</span>
+              </a>
+            </li>
+            <li className={styles.contactItem}>
+              <a href="">
+                <span>contacto</span>
+              </a>
+            </li>
             <li className={styles.contactItem}>
               <a href="">
                 <span>contacto</span>
