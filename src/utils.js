479ad8f6ef290d4c7948afa7a701fb544d30e61c
fflorezz <felipeflorezart@gmail.com>
2020-06-07 12:41:41 -0500

    added reservation page's responsive css

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const dateFormater = (date) => {
   return `${date.getFullYear()}-${
     date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
-  }-${date.getDay() + 1 < 10 ? "0" + date.getDay() : date.getDay()}`;
+  }-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
 };
